ab0680516817c48d504b1bbdc37cb7c4258ca0ec
Slibsen <elisabethkoefoed95@gmail.com>
2021-11-26 09:27:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,7 @@
-from os import read
-
-
+from os import close, read
+from user import User
+import printOut
 userFile = "/Users/ellie/Desktop/IT-Arkitektur/Afleveringer/Brugerfil.txt"
-class User:
-
-    def __init__(self, initials, name, email, password, accessCode):
-        self.initials = initials
-        self.name = name
-        self.email = email
-        self.password = password
-        self.accessCode = accessCode
-        self.file = "/Users/ellie/Desktop/IT-Arkitektur/Afleveringer/Brugerfil.txt"
-
-
-    def checkFile(self):
-        with open(self.file) as f:
-            searchFile = f.readlines()
-            for line in searchFile:
-                if self.initials in line:
-                    return True
-                if self.email in f.read():
-                    return True
-            return False
-    
-    def registerUser(self):
-        openFile = open(self.file, "a")
-        add_to_file = f"{self.initials}:{self.name}:{self.email}:{hash(self.password)}:{self.accessCode}\n" #Hash funktionen forklaring: 
-        openFile.writelines(str(add_to_file))
-        openFile.close()
-
 def printFile():
     with open(userFile) as f:
         readFile = f.readlines()
@@ -36,35 +9,29 @@ def printFile():
         wordInLine = line.split(":")
         print(f"Initialer: {wordInLine[0]}\nNavn: {wordInLine[1]}\nEmail: {wordInLine[2]}\nKodeord: {wordInLine[3]}\nProfil: {wordInLine[4]}")
 
-def getAccessLevel(initials):
-    with open(userFile) as f:
-        readFile = f.read()
-    for line in readFile:
-        wordInLine = line.split(":")
-        if initials == wordInLine[0]:
-            print(f"Profilkoden for denne medarbejder er: {wordInLine[4]}")
-            f.close()
-        else:
-            print("Medarbejderen er ikke i systemet")
-
 choice = int(input("Vil du:\n1: Registrere bruger\n2: Have et udprint af brugere\n"
                     "3: Se profil for specifik medarbejder\nVenligst indtast 1, 2 eller 3: "))
 if choice == 1:
     name = input("Indtast venligst brugerens navn: ")
-    initials = input("Indtast venligst brugerens initialer: ")
-    email = input("Indtast venligt brugerens e-mail: ")
+    initials = input("Indtast venligst brugerens initialer (Max 4 bogstaver): ")
+    if len(initials) >= 4:
+        print("")
+    email = input("Indtast venligst brugerens e-mail: ")
     password = input("Indtast venligst brugerens kodeord: ")
-    accessCode = input("Indtast venligst brugerens profilkode: ")
-    employee = initials
+    accessCode = input("Indtast venligst brugerens profilkode (Ml. 1-6): ")
+    
     employee = User(initials, name, email, password, accessCode)
     if employee.checkFile():
         print("Brugeren kan ikke oprettes, initialerne eller emailen er allerede i brug")
     else:
         print("Bruger oprettet")
         User.registerUser(employee)
+    if not employee.isProfileOK():
+        print("Profile ikke ok, skal være ml 1-6")
+    
 if choice == 2:
-    printFile()
-if choice == 3:
-    EmployeeInitials = input("Skriv initialerne på medarbejderen: ")
-    getAccessLevel(EmployeeInitials)
+    printOut.printFile()
+#if choice == 3:
+    #EmployeeInitials = input("Skriv initialerne på medarbejderen: ")
+    #User(employee).accessLevel()
 
