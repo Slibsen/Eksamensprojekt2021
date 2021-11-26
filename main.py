e923ea86cdc1cc14720fabb278864e88d83213bf
Slibsen <elisabethkoefoed95@gmail.com>
2021-11-26 15:41:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import printOut
 import createUser
-
-choice = int(input("Vil du:\n1: Registrere bruger\n2: Have et udprint af brugere\n"
+def main():
+    choice = int(input("Vil du:\n1: Registrere bruger\n2: Se et udprint af alle brugere\n"
                     "Venligst indtast 1 eller 2: "))
-if choice == 1:
-    createUser.userCreation()
+    if choice == 1:
+        createUser.userCreation()
     
-if choice == 2:
-    printOut.printFile()
+    if choice == 2:
+        printOut.printFile()
+    
+    if choice != 1 and choice != 2:
+        print("\nIkke gyldigt svar. Indtast venligst en af de viste svarmuligheder\n")
+        main()
 
+
+
+if __name__ == "__main__":
+    main()
 
