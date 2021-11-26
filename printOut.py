47f4900fa4e7975b569c2da5608f4435af29283c
Slibsen <elisabethkoefoed95@gmail.com>
2021-11-26 15:41:24 +0100

    Update printOut.py

--- printOut.py
+++ printOut.py
@@ -5,4 +5,17 @@ def printFile():
     for line in readFile:
         wordInLine = line.split(":")
         print(f"Initialer: {wordInLine[0]}\nNavn: {wordInLine[1]}\nEmail: {wordInLine[2]}\nKodeord: {wordInLine[3]}\nProfil: {wordInLine[4]}")
+
+        if int(wordInLine[4]) == 1:
+            print(f"{wordInLine[1]} har det højeste sikkerhedsniveau og kan tilgå alle systemer\n")
+        if int(wordInLine[4]) == 2:
+            print(f"{wordInLine[1]} har et højt sikkerhedsniveau og kan tilgå de fleste systemer\n")
+        if int(wordInLine[4]) == 3:
+            print(f"{wordInLine[1]} har et standard sikkerhedsniveau og kan tilgå mange systemer\n")
+        if int(wordInLine[4]) == 4:
+            print(f"{wordInLine[1]} har et begrænset sikkerhedsniveau og kan tilgå nogle systemer\n")
+        if int(wordInLine[4]) == 5:
+            print(f"{wordInLine[1]} har et meget begrænset sikkerhedsniveau og kan tilgå få systemer\n")
+        if int(wordInLine[4]) == 6:
+            print(f"{wordInLine[1]} har ikke nogen sikkerhedsgodkendelse og kan dermed ikke tilgå nogle systemer\n")
         f.close()
