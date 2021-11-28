30387505633247206e86da79ead5c4f4bd30a419
Slibsen <elisabethkoefoed95@gmail.com>
2021-11-28 13:57:41 +0100

    Update errorCatch.py

--- errorCatch.py
+++ errorCatch.py
@@ -1,10 +1,11 @@
 #Recursive funktion, prompter brugeren til at indtaste en kode mellem 1-6, og fortsætter med at prompte indtil at brugeren
 #har indtastet en gyldig værdi. Returner derefter den nye kode, som bruges i userCreation
 def newProfileCode(): 
-    newAccessCode = int(input("Profilkode skal være mellem 1-6, prøv igen: "))
+    newAccessCode = int(input("Indtast profilkode (1-6): "))
 
     if newAccessCode > 6 or newAccessCode < 0:
-        newProfileCode()
+        print(f"{newAccessCode} er ikke en gyldig værdi.")
+        return newProfileCode()
     else:
         return int(newAccessCode)
 
@@ -12,15 +13,15 @@ def newProfileCode():
 #Recursive hvis længden overskrider 4 eller er under 1. Eller hvis initialerne består af tal. 
 #Returner gyldig værdi til userCreation
 def newInitials():
-    initials = input("Initialerne er ugyldige. Prøv igen: ")
+    initials = input("Indtast initialer: ")
 
     if len(initials) > 4 or len(initials) < 1:
         print("Initialerne skal bestå af max 4 og mindst 1 bogstav.")
-        newInitials()
+        return newInitials()
     elif containsDigit(initials):
         print("Initialerne må ikke indeholde tal.")
-        return newInitials() #Her er jeg nødt til at bruge return statement af funktionen, 
-        #da den ellers returner en tom string, hvis brugeren først har indtastet initialerne med et tal 
+        return newInitials() #Jeg nødt til at bruge return statement af alle funktionerne, 
+        #da den ellers ender med at returnere None i txt filen
     else:
         return str(initials)
 
@@ -28,11 +29,21 @@ def newInitials():
 #om emailen indeholder @.
 #Returner gyldig værdi til userCreation
 def newEmail():
-    email = input("Denne email findes ikke, prøv igen: ")
+    email = input("Indtast ny e-mail: ")
     if "@" not in email:
-        newEmail()
+        print(f"{email} er ikke en gyldig e-mail.")
+        return newEmail()
     else:
         return str(email)
+
+#Recursive funktion - Tjekker om navnet indeholder tal.
+def newName():
+    name = input("Indtast navn: ")
+    if containsDigit(name):
+        print("Navnet kan ikke indeholde tal.")
+        return newName()
+    else:
+        return str(name)
 
 #Tjekker om en string indeholder et tal, så man kan give en error meddelelse i tilfælde af at
 #f.eks. navn eller initialer indeholder tal 
