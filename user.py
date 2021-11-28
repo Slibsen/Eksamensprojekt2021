7f5dbf1068ed084a6a965238d91abcd1b7fe1dce
Slibsen <elisabethkoefoed95@gmail.com>
2021-11-28 11:57:19 +0100

    Update user.py

--- user.py
+++ user.py
@@ -8,6 +8,7 @@ class User:
         self.accessCode = accessCode
         self.file = "/Users/ellie/Desktop/IT-Arkitektur/Afleveringer/Brugerfil.txt"
 
+#Tjekker om brugeren allerede er i txt filen
     def existingUser(self):
         with open(self.file) as f:
             searchFile = f.readlines()
@@ -19,25 +20,30 @@ class User:
                     return True
             f.close()
             return False
-            
+
+#Registrere brugeren i txt filen
+#Hash funktionen: 
     def registerUser(self):
         openFile = open(self.file, "a")
-        add_to_file = f"{self.initials}:{self.name}:{self.email}:{hash(self.password)}:{self.accessCode}\n" #Hash funktionen forklaring: 
+        add_to_file = f"{self.initials}:{self.name}:{self.email}:{hash(self.password)}:{self.accessCode}\n" 
         openFile.writelines(str(add_to_file))
         openFile.close()
 
+#Tjekker om profilkoden er over 6 eller under 1
     def profileOutOfBounds(self):
         if self.accessCode < 6 and self.accessCode > 0:
             return False
         else: 
             return True
-    
+
+#Tjekker om intiialerne er over 4 eller under 1, og om initialerne indeholder tal 
     def initialsOutOfBounds(self):
         if len(self.initials) <= 4 and len(self.initials) > 0 and not errorCatch.containsDigit(self.initials):
             return False
         else:
             return True
-    
+
+#Tjekker om det er en gyldig email. Dette kunne gøres på flere måder, dog valgte jeg bare at kigge efter @
     def invalidEmail(self):
         if "@" in self.email:
             return False
